5c5fa78db892f954f5634a3920d78cb5b4f49b8b
TheHarald <r.surinov@netris.ru>
2025-07-19 19:48:53 +0300

    base error handling

--- src/socket.ts
+++ src/socket.ts
@@ -51,166 +51,201 @@ export function initSocket(server: HttpServer) {
       console.log("New client connected:", socket.id);
 
       socket.on(WebGameEvents.JoinRoom, async (params) => {
-        const { userName, roomCode } = params;
+        try {
+          const { userName, roomCode } = params;
 
-        const hasAdmin = await hasRoomAdmin(roomCode);
+          const hasAdmin = await hasRoomAdmin(roomCode);
 
-        const newUser: TUser = {
-          id: socket.id,
-          name: userName,
-          isAdmin: !hasAdmin,
-        };
+          const newUser: TUser = {
+            id: socket.id,
+            name: userName,
+            isAdmin: !hasAdmin,
+          };
 
-        await addUserToRoom(roomCode, newUser);
+          await addUserToRoom(roomCode, newUser);
 
-        const room = await getRoom(roomCode);
-
-        if (!room) return;
-
-        socket.join(roomCode);
-
-        io.to(socket.id).emit(WebGameEvents.MyUserJoined, newUser);
-
-        // Notify room about new user
-        io.to(roomCode).emit(WebGameEvents.UserJoined, room);
-
-        console.log(`${userName} joined to room ${roomCode}`);
-      });
-
-      socket.on(WebGameEvents.CreateRoom, async (userName) => {
-        const newUser: TUser = {
-          id: socket.id,
-          name: userName,
-          isAdmin: true,
-        };
-
-        const newRoomCode = generatateId();
-
-        const newRoom: TRoom = {
-          roomCode: newRoomCode,
-          memes: [],
-          state: WebGameStates.WaitStart,
-          users: [],
-        };
-
-        await addRoom(newRoom);
-
-        await addUserToRoom(newRoomCode, newUser);
-
-        const room = await getRoom(newRoomCode);
-
-        if (!room) return;
-
-        socket.join(newRoomCode);
-
-        io.to(socket.id).emit(WebGameEvents.MyUserJoined, newUser);
-
-        // Notify room about new user
-        io.to(newRoomCode).emit(WebGameEvents.UserJoined, room);
-
-        console.log(`${userName} created room ${newRoomCode} and join`);
-      });
-
-      socket.on(WebGameEvents.LeaveRoom, async (params) => {
-        const { roomCode, userId } = params;
-
-        await deleteUserFromRoom(roomCode, userId);
-
-        const room = await getRoom(roomCode);
-
-        if (!room) return;
-
-        // Notify room about user leaving
-        io.to(roomCode).emit(WebGameEvents.UserLeft, room);
-
-        socket.leave(roomCode);
-
-        console.log(`${userId} left room ${roomCode}`);
-      });
-
-      socket.on(WebGameEvents.SendPoo, (userId) => {
-        console.log("poo send to", userId);
-        io.to(userId).emit(WebGameEvents.RecivePoo);
-      });
-
-      socket.on(WebGameEvents.SendMessage, (params) => {
-        io.to(params.roomCode).emit(
-          WebGameEvents.ReciveMessage,
-          params.message
-        );
-      });
-
-      socket.on(WebGameEvents.ChangeGameState, async ({ roomCode, state }) => {
-        console.log(roomCode, "start game");
-
-        await patchRoom(roomCode, {
-          state,
-        });
-
-        if (state === WebGameStates.CreatingImage) {
           const room = await getRoom(roomCode);
 
           if (!room) return;
 
-          const newMemes = shuffleAndAssignMemeRecipients(room.memes);
+          socket.join(roomCode);
+
+          io.to(socket.id).emit(WebGameEvents.MyUserJoined, newUser);
+
+          // Notify room about new user
+          io.to(roomCode).emit(WebGameEvents.UserJoined, room);
+          console.log(`${userName} joined to room ${roomCode}`);
+        } catch (e) {
+          io.to(socket.id).emit(WebGameEvents.SomeError, "Some Error");
+        }
+      });
+
+      socket.on(WebGameEvents.CreateRoom, async (userName) => {
+        try {
+          const newUser: TUser = {
+            id: socket.id,
+            name: userName,
+            isAdmin: true,
+          };
+
+          const newRoomCode = generatateId();
+
+          const newRoom: TRoom = {
+            roomCode: newRoomCode,
+            memes: [],
+            state: WebGameStates.WaitStart,
+            users: [],
+          };
+
+          await addRoom(newRoom);
+
+          await addUserToRoom(newRoomCode, newUser);
+
+          const room = await getRoom(newRoomCode);
+
+          if (!room) return;
+
+          socket.join(newRoomCode);
+
+          io.to(socket.id).emit(WebGameEvents.MyUserJoined, newUser);
+
+          // Notify room about new user
+          io.to(newRoomCode).emit(WebGameEvents.UserJoined, room);
+
+          console.log(`${userName} created room ${newRoomCode} and join`);
+        } catch (e) {
+          io.to(socket.id).emit(WebGameEvents.SomeError, "Some Error");
+        }
+      });
+
+      socket.on(WebGameEvents.LeaveRoom, async (params) => {
+        try {
+          const { roomCode, userId } = params;
+
+          await deleteUserFromRoom(roomCode, userId);
+
+          const room = await getRoom(roomCode);
+
+          if (!room) return;
+
+          // Notify room about user leaving
+          io.to(roomCode).emit(WebGameEvents.UserLeft, room);
+
+          socket.leave(roomCode);
+
+          console.log(`${userId} left room ${roomCode}`);
+        } catch (e) {
+          io.to(socket.id).emit(WebGameEvents.SomeError, "Some Error");
+        }
+      });
+
+      socket.on(WebGameEvents.SendPoo, (userId) => {
+        try {
+          console.log("poo send to", userId);
+          io.to(userId).emit(WebGameEvents.RecivePoo);
+        } catch (e) {
+          io.to(socket.id).emit(WebGameEvents.SomeError, "Some Error");
+        }
+      });
+
+      socket.on(WebGameEvents.SendMessage, (params) => {
+        try {
+          io.to(params.roomCode).emit(
+            WebGameEvents.ReciveMessage,
+            params.message
+          );
+        } catch (e) {
+          io.to(socket.id).emit(WebGameEvents.SomeError, "Some Error");
+        }
+      });
+
+      socket.on(WebGameEvents.ChangeGameState, async ({ roomCode, state }) => {
+        try {
+          console.log(roomCode, "start game");
 
           await patchRoom(roomCode, {
-            memes: newMemes,
+            state,
           });
+
+          if (state === WebGameStates.CreatingImage) {
+            const room = await getRoom(roomCode);
+
+            if (!room) return;
+
+            const newMemes = shuffleAndAssignMemeRecipients(room.memes);
+
+            await patchRoom(roomCode, {
+              memes: newMemes,
+            });
+          }
+
+          const shufledRoom = await getRoom(roomCode);
+
+          if (!shufledRoom) return;
+
+          io.to(roomCode).emit(WebGameEvents.GameStateChanged, shufledRoom);
+        } catch (e) {
+          io.to(socket.id).emit(WebGameEvents.SomeError, "Some Error");
         }
-
-        const shufledRoom = await getRoom(roomCode);
-
-        if (!shufledRoom) return;
-
-        io.to(roomCode).emit(WebGameEvents.GameStateChanged, shufledRoom);
       });
 
       socket.on(WebGameEvents.CreateImage, async ({ meme, roomCode }) => {
-        console.log("create image", meme, roomCode);
+        try {
+          console.log("create image", meme, roomCode);
 
-        await updateMemeInRoom(roomCode, meme);
+          await updateMemeInRoom(roomCode, meme);
 
-        const room = await getRoom(roomCode);
+          const room = await getRoom(roomCode);
 
-        console.log(room);
+          console.log(room);
 
-        if (!room) return;
+          if (!room) return;
 
-        io.to(roomCode).emit(WebGameEvents.ImageCreated, room);
+          io.to(roomCode).emit(WebGameEvents.ImageCreated, room);
+        } catch (e) {
+          io.to(socket.id).emit(WebGameEvents.SomeError, "Some Error");
+        }
       });
 
       socket.on(WebGameEvents.CreateMeme, async ({ meme, roomCode }) => {
-        console.log("Meme created", meme, roomCode);
+        try {
+          console.log("Meme created", meme, roomCode);
 
-        await updateMemeInRoom(roomCode, meme);
+          await updateMemeInRoom(roomCode, meme);
 
-        const room = await getRoom(roomCode);
+          const room = await getRoom(roomCode);
 
-        console.log(room);
+          console.log(room);
 
-        if (!room) return;
+          if (!room) return;
 
-        io.to(roomCode).emit(WebGameEvents.MemeCreated, room);
+          io.to(roomCode).emit(WebGameEvents.MemeCreated, room);
+        } catch (e) {
+          io.to(socket.id).emit(WebGameEvents.SomeError, "Some Error");
+        }
       });
 
       socket.on(WebGameEvents.RestartGame, async ({ roomCode }) => {
-        const room = await getRoom(roomCode);
+        try {
+          const room = await getRoom(roomCode);
 
-        if (!room) return;
+          if (!room) return;
 
-        const clearedMemes = shuffleAndAssignMemeRecipients(room.memes, true);
+          const clearedMemes = shuffleAndAssignMemeRecipients(room.memes, true);
 
-        await patchRoom(roomCode, {
-          memes: clearedMemes,
-          state: WebGameStates.WaitStart,
-        });
+          await patchRoom(roomCode, {
+            memes: clearedMemes,
+            state: WebGameStates.WaitStart,
+          });
 
-        const clearedRoom = await getRoom(roomCode);
+          const clearedRoom = await getRoom(roomCode);
 
-        if (!clearedRoom) return;
+          if (!clearedRoom) return;
 
-        io.to(roomCode).emit(WebGameEvents.GameStateChanged, clearedRoom);
+          io.to(roomCode).emit(WebGameEvents.GameStateChanged, clearedRoom);
+        } catch (e) {
+          io.to(socket.id).emit(WebGameEvents.SomeError, "Some Error");
+        }
       });
 
       socket.on(WebGameEvents.Disconnect, async () => {
